222827b6aae41289499e50d9ed585a317d3b105e
ademariocarvalho <ademariocarvalho@yahoo.com.br>
2021-06-17 13:11:56 -0300

    commit de mudança idiota

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(nome):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8   to toggle the breakpoint.
+    print(f'Hi, {nome}')  # Press Ctrl+F8   to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
