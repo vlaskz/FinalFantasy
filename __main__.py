af1e74f0a9aba2e66f63e088f81166452ae25cf8
ademariocarvalho <ademariocarvalho@yahoo.com.br>
2021-06-18 09:47:20 -0300

    teste com a biblioteca PIL

---
+++ __main__.py
@@ -0,0 +1,21 @@
+from PIL import Image, ImageFilter
+#Read image
+im = Image.open('pcb.jpg')
+#Display image
+im.show()
+
+# ou o códido a seguir
+
+import HYP_Utils
+import sys
+
+from PIL import Image
+
+scriptDir = HYP_Utils.GetDemoDir()
+
+PIL_Version = Image.VERSION
+
+img_filename = "%s/pcb.jpg" % scriptDir
+im = Image.open(img_filename)
+im.show()
+
